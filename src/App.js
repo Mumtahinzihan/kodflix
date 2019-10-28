ade8f03a1de0e863f5456796f1bf2c5767e073b4
Mumtahinzihan <mumtahin@yahoo.com>
2019-10-28 16:51:23 +0000

    update App.js with message

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        My First kodflix Project
+      </p>
+        <div className= 'container'>Movie1</div>
+        <div className= 'container'>Movie1</div>
+        <div className= 'container'>Movie1</div>
+        <div className= 'container'>Movie1</div>
+        <div className= 'container'>Movie1</div>
+        <div className= 'container'>Movie1</div>
+
+    </div>
+  );
+}
+
+export default App;
