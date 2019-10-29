1243661399e0f79640904eedc97e80d8bf426044
Mumtahinzihan <mumtahin@yahoo.com>
2019-10-29 11:29:49 +0000

    update jsx h1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import joker from './images/jokerMovieCover.png';
 function App() {
   return (
     <div className="App">
-      <h1>WELCOME TO KODFLIX</h1>
+      <h1>WELCOME TO KODFLIX!</h1>
       <div className='movies-of-the-year'>
         <div className='horror-movie container'>
           <div className='movie'><h3>Joker</h3> <img src={joker} alt="jokes"/></div>
